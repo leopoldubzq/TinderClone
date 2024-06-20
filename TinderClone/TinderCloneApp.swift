57245500292ff9a376b6d4c15ac507c7a7792f5a
Leopold Romanowski <leopold.romanowski@gmail.com>
2024-06-20 16:10:53 +0200

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Leopold Romanowski on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
